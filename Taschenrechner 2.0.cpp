5ddcf6c07787177165572235d2c1794ee0dfba37
Lucio263 <69530165+Lucio263@users.noreply.github.com>
2022-02-16 10:49:26 +0100

    Add files via upload

---
+++ Taschenrechner 2.0.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+/*
+-Menü mit void und so
+-verlauf in Datei speichern
+-verschiedene Berechnungen ermöglichen, vllt auch welche von Physik
+*/
+
+void print_menue()
+{
+    cout << setw(20) << setfill("~") << left << endl;
+    cout << cout << cout << cout <<
+}
+
+int main()
+{
+    // Letsgoo
+}
